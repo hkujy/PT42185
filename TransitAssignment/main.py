027a219478393f63afda4393825a5636e7a1c233
phdjiangyu@outlook.com <phdjiangyu@outlook.com>
2019-02-13 22:09:50 +0000

    input data

--- TransitAssignment/main.py
+++ TransitAssignment/main.py
@@ -1,27 +1,39 @@
 """
 Transit Assignment
 """
-
+LARGE_FRE = 999999.9 # a large number for the frequency on the walking link
+class bus_line_class():
+    def __init__(self,name,id,fre):
+        self.name = name
+        self.id = id
+        self.fre = fre
+        self.stops = []
+    pass
 class node_class():
     def __init__(self, node_name, node_id):
         self.id = node_id
         self.name = node_name
         self.out_links = []
         self.in_links = []
+        self.lines = []
     pass
 
 class link_class():
-    def __init__(self,link_name, link_id):
+    def __init__(self,link_name,link_id,cost):
         self.id = link_id
         self.name = link_name
         self.tail_node = []  
         self.head_node = []
+        self.cost = cost
+        self.lines = []
+        self.fre = -1.0
     pass
 
 class graph_class():
     def __init__(self):
         self.links = []
         self.nodes = []
+        self.lines = []
     pass
 
 
@@ -31,6 +43,13 @@ def input_network():
     """
     # create graph
     graph = graph_class()
+    # add lines 
+    graph.lines.append(bus_line_class("G1",0, 1/6))
+    graph.lines.append(bus_line_class("G2",1, 1/6))
+    graph.lines.append(bus_line_class("G3",0.07, 1/15))
+
+
+
     # input nodes
     graph.nodes.append(node_class("A",0))
     graph.nodes.append(node_class("X2",1))  
@@ -40,7 +59,7 @@ def input_network():
     graph.nodes.append(node_class("Y3",5))  
 
     # input links
-    graph.links.append(link_class("A-B",0))
+    graph.links.append(link_class("A-B",0,25))
     graph.links.append(link_class("A-X2",1))
     graph.links.append(link_class("X2-Y",2))
     graph.links.append(link_class("Y-B",3))
