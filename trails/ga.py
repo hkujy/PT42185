db8ba7801d85aa44a28d48b3cdd95e55646328b7
hkujy <phdjiangyu@outlook.com>
2019-02-20 15:19:22 +0100

    Thinkpad: 20-Feb-2019

---
+++ trails/ga.py
@@ -0,0 +1,51 @@
+"""
+    The code is created to test the ga algorithm in python
+"""
+import array
+import random
+
+import numpy
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", array.array, typecode='b', fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+# Attribute generator
+toolbox.register("attr_bool", random.randint, 0, 1)
+
+# Structure initializers
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalOneMax(individual):
+    return sum(individual),
+
+toolbox.register("evaluate", evalOneMax)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+def main():
+    random.seed(64)
+    
+    pop = toolbox.population(n=300)
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+    
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40, 
+                                   stats=stats, halloffame=hof, verbose=True)
+    
+    return pop, log, hof
+
+if __name__ == "__main__":
+    main()
